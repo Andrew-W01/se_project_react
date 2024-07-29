98fc7fe4810bda2b9edcbde57d6399a0d29b228b
Andrew.Worley <drewjw1212@gmail.com>
2024-07-28 21:11:41 -0400

    project 10

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 function App() {
   return (
     <div className="page">
       <div className="page__content">
         <Header />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__author">Developed by Andrew Worley</p>
+      <p className="footer__date">$copy; 2024</p>
+    </div>
+  );
+}
+
+export default Footer;
