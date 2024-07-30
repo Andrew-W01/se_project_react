ba5a27a71e1e0e253b957455f8dbea2ee874d094
Andrew.Worley <drewjw1212@gmail.com>
2024-07-30 19:54:33 -0400

    project 10

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -36,3 +36,10 @@ export const defaultClothingItems = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
   },
 ];
+
+export const coordinates = {
+  latitude: 27.950575,
+  longitude: -82.457176,
+};
+
+export const APIkey = "64c8ca6be7e56d0d2a39cb12ea9e9f4d";
